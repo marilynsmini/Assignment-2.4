de8c86b3c80cdcac2217a1ac923be5a16312837f
marilynsmini <122817758+marilynsmini@users.noreply.github.com>
2024-09-06 17:53:51 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,26 @@
+
+
+const level = {color: "red"};
+level.color = "Yellow"
+
+
+document.getElementById("colorScale").innerHTML = "Alert Level = " + level.color; 
+
+var description = "Check the boxes to make them disappear and you list count go down!";
+document.getElementById("description").innerHTML = description;
+//Global Variable
+
+
+{let a, b, c, d, e;
+a = 1;
+b = 1;
+c = 1;
+d = 1;
+e = 1;
+f = a + b + c + d + e;
+//Block Variable
+}
+
+
+document.getElementById("listCount").innerHTML =
+"Total = " + f + "";
